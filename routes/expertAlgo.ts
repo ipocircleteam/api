c5c9e398a65e5a08f562fe56d2f0a801488df615
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-12-02 13:07:14 +0530

    request validator

--- routes/expertAlgo.ts
+++ routes/expertAlgo.ts
@@ -1,6 +1,17 @@
-import express from 'express'
-import { runAlgo } from '../controllers/algo-controllers'
+import express from "express";
+import { runAlgo } from "../controllers/algo-controllers";
+import { check } from "express-validator";
+import validateRequest from "../utils/req-validator";
 
-export const algoRouter = express.Router()
+export const algoRouter = express.Router();
 
-algoRouter.post("/run", runAlgo)
+algoRouter.post(
+  "/run",
+  [
+    check("amount").notEmpty().isNumeric(),
+    check("no_of_demat_acc").notEmpty().isNumeric(),
+    check("risk_profile").notEmpty().isNumeric(),
+  ],
+  validateRequest,
+  runAlgo
+);
