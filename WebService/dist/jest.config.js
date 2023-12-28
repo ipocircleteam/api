dea609ba8e15dd0e195bac960586f770409f1095
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-12-28 16:58:06 +0530

    updated docker files

--- WebService/dist/jest.config.js
+++ WebService/dist/jest.config.js
@@ -3,5 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jest_config_1 = require("jest-config");
 const config = {
     moduleFileExtensions: [...jest_config_1.defaults.moduleFileExtensions, "mts"],
+    testMatch: [
+        '__tests__/*.ts?(x)',
+        '__tests__/**/*.ts?(x)',
+        '__tests__/**/*.tsx?(x)',
+        '?(*.)+(spec|test).ts?(x)',
+        '?(*.)+(spec|test).tsx?(x)',
+    ],
+    testPathIgnorePatterns: ['<rootDir>/dist/*.js'],
 };
 exports.default = config;
--- WebService/jest.config.ts
+++ WebService/jest.config.ts
@@ -3,6 +3,14 @@ import { defaults } from "jest-config";
 
 const config: Config = {
   moduleFileExtensions: [...defaults.moduleFileExtensions, "mts"],
+  testMatch: [
+    '__tests__/*.ts?(x)',
+    '__tests__/**/*.ts?(x)',
+    '__tests__/**/*.tsx?(x)',
+    '?(*.)+(spec|test).ts?(x)',
+    '?(*.)+(spec|test).tsx?(x)',
+  ],
+  testPathIgnorePatterns: ['<rootDir>/dist/*.js'],
 };
 
 export default config;
